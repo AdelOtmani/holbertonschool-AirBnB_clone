df74afbfac527e983e3bf9b82b3d70e17b433a81
AdelOtmani <5023@holbertonschool.com>
2022-10-13 16:30:33 +0200

    Console.py AirBnB

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Project AirBnB holberton A. Otmani
+    """
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Console initialisation Hbnb
+    """
+    prompt = '(hbnb)'
+
+    def do_EOF(self, line):
+        """End Of File
+        """
+        print()
+        return True
+
+    def do_emptyline(self):
+        """empty line"""
+
+    def do_quit(self, line):
+        """Quit command to exit the program
+        """
+        quit()
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()

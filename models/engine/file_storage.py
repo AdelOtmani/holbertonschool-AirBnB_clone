9d58602e87cd2c034738f1bdbfe1a321a07f801c
AdelOtmani <5023@holbertonschool.com>
2022-10-13 15:11:12 +0200

    Exercice 5 airBnB A. Otmani

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,23 +17,22 @@ class FileStorage():
         return self.__objects
 
     def new(self, obj):
-        FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
         obj_json = {}
+        for keyK in self.__objects:
+            obj_json[keyK] = self.__objects[keyK].to_dict()
         with open(FileStorage.__file_path, 'w') as My_file:
-            for key, value in self.__dict__.items():
-                obj_json.update([key].to_dict(key))
             json.dump(obj_json, My_file)
 
     def reload(self):
         try:
-            with open(FileStorage.__file_path, 'r') as My_file:
-                strjsn = json.loads(My_file.read())
-                for keyK, keyV in strjsn.items():
-                    new_dict = eval("{}(**keyV)".format(keyV["__class__"]))
-
-
+            with open(self.__file_path, 'r') as My_file:
+                    strjsn = json.loads(My_file.read())
+                    for keyK, keyV in strjsn.items():
+                        new_dict = eval((keyV.get("__class__") + "(**keyV)"))
+                        FileStorage.__objects[keyK] = new_dict
         except FileNotFoundError:
             pass
 

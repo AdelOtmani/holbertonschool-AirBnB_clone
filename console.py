d711d1d8f2a624514fa9164505ea5a30ed96a6c5
AdelOtmani <5023@holbertonschool.com>
2022-10-14 17:06:45 +0200

    Console.py project airBnB A.Otmani

--- console.py
+++ console.py
@@ -2,6 +2,9 @@
 """Project AirBnB holberton A. Otmani
     """
 import cmd
+import json
+import sys
+from models import *
 
 class HBNBCommand(cmd.Cmd):
     """Console initialisation Hbnb
@@ -23,7 +26,118 @@ class HBNBCommand(cmd.Cmd):
         """
         quit()
 
+    def do_create(self, arg):
+        """"""
+        if arg is None:
+            print("** class name missing **")
+            return False
+        try:
+            model_class = getattr(sys.modules[__name__], arg)
+        except Exception:
+            print("** class doesn't exist **")
+            return False
 
+        obj = model_class()
+        obj.save()
+        print(obj.id)
+
+    def do_show(self, line):
+        """ Prints the string representation of an instance based on the class name and id"""
+        if line is None:
+            print("** class name missing **")
+        args = line.split()
+        try:
+            model_class = getattr(sys.modules[__name__], args[0])
+        except Exception:
+            print("** class doesn't exist **")
+            return False
+        if len(args) < 2:
+            print("** instance id missing **")
+            return False
+        objs = storage.all()
+        obj = objs.get('.'.join(args), None)
+        if obj is None:
+            print("** no instance found **")
+        else:
+            print(obj)
+
+    def do_destroy(self, line):
+        """_summary_
+
+        Args:
+            line (_type_): _description_
+        """
+        if line is None:
+            print("** class name missing **")
+        args = line.split()
+        try:
+            model_class = getattr(sys.modules[__name__], args[0])
+        except Exception:
+            print("** class doesn't exist **")
+            return False
+        if len(args) < 2:
+            print("** instance id missing **")
+            return False
+        objs = storage.all()
+        obj = objs.get('.'.join(args), None)
+        if obj is None:
+            print("** no instance found **")
+        else:
+            del objs['.'.join(args)]
+            storage.save()
+
+    def do_all(self, line):
+        """_summary_
+
+        Args:
+            line (_type_): _description_
+        """
+        if line is None:
+            print("** class name missing **")
+        args = line.split()
+        try:
+            model_class = getattr(sys.modules[__name__], args[0])
+        except Exception:
+            print("** class doesn't exist **")
+            return False
+
+        objs = storage.all()
+        print(
+            [str(obj) for obj in objs.values() if type(obj) is model_class]
+        )
+
+    def do_update(self, line):
+        """
+
+        Args:
+            line (_type_): _description_
+        """
+
+
+        args = line.split()
+        objs = storage.all()
+        try:
+            argsLen = len(args)
+            obj = objs.get('.'.join([args[0], args[1]]), None)
+            if (obj is None or argsLen <= 3):
+                raise
+        except Exception:
+            if argsLen <= 0:
+                print("** class name missing **")
+            elif args[0] not in str(list(objs.keys())):
+                print("** class doesn't exist **")
+            elif argsLen == 1:
+                print("** instance id missing **")
+            elif obj is None:
+                print("** no instance found **")
+            elif argsLen == 2:
+                print("** attribute name missing **")
+            elif argsLen == 3:
+                print("** value missing **")
+            return
+
+        setattr(obj, args[2], type(getattr(obj, args[2]))(args[3]) if args[2] in obj.to_dict() else args[3])
+        storage.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
